39ec472e9a114583d1e67528f324b944ce62d62c
Mayur Tarlekar <146643570+mayur-tarlekar3@users.noreply.github.com>
2026-03-06 17:30:21 +0530

    Enhance tic-tac-toe game with reset and draw features
    Refactored game logic to include reset and draw functionality.

--- app.js
+++ app.js
@@ -1,50 +1,92 @@
-let box=document.querySelectorAll(".box");
 
-let trunO=true;  
-let winPatterns=[
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; //playerX, playerO
+let count = 0; //To Track Draw
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
 ];
 
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
 
-box.forEach(b => {
-  b.addEventListener("click", ()=>{
-  if(trunO){
-      b.innerText="O";
-      trunO=false;
-  }else{
-          b.innerText="X";
-      trunO=true;
-  }
-  
-   b.disabled=true;
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      //playerO
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      //playerX
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    count++;
 
-   checkWinner();
+    let isWinner = checkWinner();
 
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
   });
-    
 });
 
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
 
-const checkWinner=()=>{
-    for(let pattern of winPatterns){
-       
-        
-       let posi1=box[pattern[0]].innerText;
-       let posi2=box[pattern[1]].innerText;   // each pattern on box (box array means elements in box class 0 to 9 index wise so pattern are filled in as index to check wipattern)
-       let posi3= box[pattern[2]].innerText;  // so basically on each winning patterns postion find outed herer like which box or button and inner text for next checking
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
 
-       if(posi1 !="" && posi2 !="" && posi3 !=""){
-        if(posi1 === posi2 && posi2 === posi3){
-            console.log("winnerr");
-        }
-       }
-        
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return true;
+      }
     }
-}
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
